afb1f765285f284692e80218d7e2572aa5f256d4
Jin <lorehes4892@gmail.com>
2026-03-27 16:20:25 +0900

    Add Selenium script for Instagram login and hashtag search automation

---
+++ selenium_insta/03_search_insta.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import chromedriver_autoinstaller
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"))
+USERNAME = os.getenv("id")
+PASSWORD = os.getenv("pw")
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists and is compatible with the installed Chrome browser. If not, it will download and install the latest version.
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")  # Hide automation flag
+options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Remove automation banner
+options.add_experimental_option("useAutomationExtension", False)  # Disable automation extension
+
+driver = webdriver.Chrome(options=options)  # Create a new instance of the Chrome driver
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")  # Hide webdriver property
+
+url = "https://www.instagram.com/accounts/login/"  # Direct login URL
+driver.get(url)  # Open the Instagram website
+
+time.sleep(5)  # Wait for login form to render
+input_id = driver.find_element(By.NAME, "email")  # Find the username input field
+input_id.click()  # Focus the input field
+input_id.send_keys(USERNAME)  # Enter the username
+
+input_pw = driver.find_element(By.NAME, "pass")  # Find the password input field
+input_pw.click()  # Focus the input field
+input_pw.send_keys(PASSWORD)  # Enter the password
+input_pw.send_keys(Keys.RETURN)  # Submit the form by pressing Enter
+time.sleep(10)  # Wait for login to complete
+
+
+# search
+hastag = "인스타그램"  # Hashtag to search for
+url = f'https://www.instagram.com/explore/tags/{hastag}/'  # URL of the Instagram search page for the hashtag "인스타그램"
+driver.get(url=url)  # Open the Instagram search page
+
+time.sleep(299)  # Wait for login to complete
