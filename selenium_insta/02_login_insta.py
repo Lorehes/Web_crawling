00e6baf328fe133721363fa41179a443572311eb
Jin <lorehes4892@gmail.com>
2026-03-27 16:13:07 +0900

    Refactor Selenium login script for Instagram: uncomment options and driver initialization

--- selenium_insta/02_login_insta.py
+++ selenium_insta/02_login_insta.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import chromedriver_autoinstaller
 import time
 import os
@@ -11,13 +12,13 @@ PASSWORD = os.getenv("pw")
 
 chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists and is compatible with the installed Chrome browser. If not, it will download and install the latest version.
 
-# options = webdriver.ChromeOptions()
-# options.add_argument("--disable-blink-features=AutomationControlled")  # Hide automation flag
-# options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Remove automation banner
-# options.add_experimental_option("useAutomationExtension", False)  # Disable automation extension
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")  # Hide automation flag
+options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Remove automation banner
+options.add_experimental_option("useAutomationExtension", False)  # Disable automation extension
 
-# driver = webdriver.Chrome(options=options)  # Create a new instance of the Chrome driver
-# driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")  # Hide webdriver property
+driver = webdriver.Chrome(options=options)  # Create a new instance of the Chrome driver
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")  # Hide webdriver property
 
 url = "https://www.instagram.com/accounts/login/"  # Direct login URL
 driver.get(url)  # Open the Instagram website
@@ -26,8 +27,12 @@ time.sleep(5)  # Wait for login form to render
 input_id = driver.find_element(By.NAME, "email")  # Find the username input field
 input_id.click()  # Focus the input field
 input_id.send_keys(USERNAME)  # Enter the username
+print("입력된 이메일:", input_id.get_attribute("value"))  # Verify email input
 
 input_pw = driver.find_element(By.NAME, "pass")  # Find the password input field
 input_pw.click()  # Focus the input field
 input_pw.send_keys(PASSWORD)  # Enter the password
-time.sleep(20)  # Wait for 20 seconds to see the opened page
+input_pw.send_keys(Keys.RETURN)  # Submit the form by pressing Enter
+
+time.sleep(10)  # Wait for login to complete
+print("현재 URL:", driver.current_url)  # Print the current URL to verify login
